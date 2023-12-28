694e2e53a54bbf09f9aa32014243e7947df8de6f
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-28 18:25:47 +0530

    Update provider.tf

--- terraform/accounts/dev/ecs/provider.tf
+++ terraform/accounts/dev/ecs/provider.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
+    }
+  }
+
+  backend "s3" {
+    
+  }
+}
+
 provider "aws" {
   region  = "us-east-1"
   default_tags {

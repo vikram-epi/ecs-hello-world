5ea15291e843db92e0d52631125deed71bf8a2fc
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-29 18:24:45 +0530

    Update provider.tf

--- terraform/accounts/dev/remote_state/provider.tf
+++ terraform/accounts/dev/remote_state/provider.tf
@@ -1,10 +1,7 @@
-provider "aws" {
-  region  = "us-east-1"
-  profile = "default"
-  default_tags {
-    tags = {
-      Organisation = "self"
-      Environment  = "dev"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
     }
   }
-}

87d07e7d4b4b588d008804726d9dc749d5f81795
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-29 18:27:34 +0530

    Update provider.tf

--- terraform/accounts/dev/remote_state/provider.tf
+++ terraform/accounts/dev/remote_state/provider.tf
@@ -2,6 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.18.0"
+      version = "~> 5.0"
     }
   }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+

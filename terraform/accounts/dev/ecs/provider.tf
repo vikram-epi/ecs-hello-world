73fc5469aa4a9c8d475e94ac7f312338f4414bfd
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-29 23:52:30 +0530

    Update provider.tf

--- terraform/accounts/dev/ecs/provider.tf
+++ terraform/accounts/dev/ecs/provider.tf
@@ -5,12 +5,16 @@ terraform {
       version = "~> 5.0"
     }
   }
-}
-backend "s3" {
-    
+
+  backend "s3" {
+    bucket         	   = "hello-world-terraform-state-morrisons-demo"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "us-east-1"
+    encrypt        	   = true
+    dynamodb_table = "mycomponents_tf_lockid"
   }
+}
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-}
 }

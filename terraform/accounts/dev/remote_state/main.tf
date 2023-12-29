5f05fca9ffd002364cec35edb2214309a1232b51
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-12-29 19:01:08 +0530

    Update main.tf

--- terraform/accounts/dev/remote_state/main.tf
+++ terraform/accounts/dev/remote_state/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "hello-world-terraform-state"
 
